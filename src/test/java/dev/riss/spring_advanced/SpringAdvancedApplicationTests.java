4913826b65c06461d7bcb0d05874ada61687385c
이경환 <khlee@rsupport.com>
2025-05-09 15:38:57 +0900

    first commit for setting

---
+++ src/test/java/dev/riss/spring_advanced/SpringAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.riss.spring_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
